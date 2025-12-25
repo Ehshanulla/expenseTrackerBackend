cc59f1df9c02fcee6827093efa4b340848c026e0
Ehshanulla <tappalehshanulla@gmail.com>
2025-12-25 13:54:02 +0530

    added the ds service

---
+++ dsService/src/app/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask
+import json
+from flask import request, jsonify
+from service.messageService import MessageService
+from kafka import KafkaProducer
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+messageService = MessageService()
+
+messageService = MessageService()
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+@app.route('/v1/ds/message', methods=['POST'])
+def handle_message():
+    message = request.json.get('message')
+    result = messageService.process_message(message)
+    serialized_result = result.json()
+
+    
+    producer.send('expense_service', serialized_result)
+    return jsonify(result)
+
+@app.route('/', methods=['GET'])
+def handle_get():
+    return 'Hello world'
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port= 8000 ,debug=True)
